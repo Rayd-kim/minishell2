914011ada44c3ae5d8ceba9ad0a8ff8e23a49f77
Yousung Kim <youskim@c10r3s14.42seoul.kr>
2022-06-24 21:43:34 +0900

    0624

--- change_input.c
+++ change_input.c
@@ -102,7 +102,8 @@ char	*check_env_vari(char *str, t_list *env)
 		while (temp != NULL)
 		{
 			if (ft_strncmp(path, temp->str, path_len(temp->str)) == 0)
-				return (check_env_vari(set_env_vari(str, temp, path), env));
+				return (set_env_vari(str, temp, path));
+				// return (check_env_vari(set_env_vari(str, temp, path), env));
 			temp = temp->next;
 		}
 		return (set_env_empty(str, path));
@@ -139,32 +140,91 @@ char	*remove_quote(char *str, int type)
 	return (ret);
 }
 
-char	*change_quote(char *str, t_list *env)
+char	*check_env(char *str, t_list *env)
 {
+	int		quote_single;
 	int		i;
-	char	*temp;
-	char	*ret;
 
 	i = 0;
-	while (str[i] != '\0')
+	quote_single = 0;
+	while(str[i])
 	{
-		if (str[i] == '\"')
-		{
-			temp = check_env_vari(str, env);
-			if (temp != str)
-			{
-				ret = remove_quote(temp, 2);
-				free(temp);
-				return (ret);
-			}
-			else
-				return (remove_quote(temp, 2));
-		}
-		else if (str[i] == '\'')
-			return (remove_quote(str, 1));
-		else if (str[i] == '$')
+		if (str[i] == '\'')
+			quote_single++;
+		else if (str[i] == '$' && quote_single % 2 == 0)		
 			return (check_env_vari(str, env));
 		i++;
 	}
 	return (str);
 }
+
+char	*change_quote(char *str, t_list *env)
+{
+	int		i;
+	char	*temp;
+	char	*free_temp;
+	char	*ret;
+
+	temp = check_env(str, env);
+	if (temp != str)
+	{
+		while (1)
+		{
+			free_temp = temp;
+			temp = check_env(temp, env);
+			if (free_temp == temp)
+			{
+				// free(free_temp);
+				break ;
+			}
+			free(free_temp);
+		}
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\"')
+		{
+			ret = remove_quote(temp, 2);
+			// free (free_temp);
+			return (ret);
+		}
+			// return (remove_quote(temp, 2));
+		else if (str[i] == '\'')
+			return (remove_quote(temp, 1));
+		i++;
+	}
+	return (temp);
+}
+
+
+
+// char	*change_quote(char *str, t_list *env)
+// {
+// 	int		i;
+// 	char	*temp;
+// 	char	*ret;
+
+// 	i = 0;
+// 	while (str[i] != '\0')
+// 	{
+// 		if (str[i] == '\"')
+// 		{
+// 			temp = check_env_vari(str, env);
+// 			if (temp != str)
+// 			{
+// 				ret = remove_quote(temp, 2);
+// 				free(temp);
+// 				return (ret);
+// 			}
+// 			else
+// 				return (remove_quote(temp, 2));
+// 		}
+// 		else if (str[i] == '\'')
+// 			return (remove_quote(str, 1));
+// 		else if (str[i] == '$')
+// 			return (check_env_vari(str, env));
+// 		i++;
+// 	}
+// 	return (str);
+// }
